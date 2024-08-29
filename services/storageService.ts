6e7ec50e5344df9eb72bf5dc7d08ea4a26b4cdc0
Lucas William <lucaswilliamml@gmail.com>
2024-08-29 20:14:07 -0300

    comment change

--- services/storageService.ts
+++ services/storageService.ts
@@ -3,16 +3,15 @@ import path from 'path';
 import { v4 as uuidv4 } from 'uuid';
 
 export const uploadToLocalStorage = (base64Data: string, mimeType: string): string => {
-  // Remove o prefixo data:image/...;base64, se estiver presente
+  // Remove o prefixo
   const base64String = base64Data.replace(/^data:image\/\w+;base64,/, '');
   
   const buffer = Buffer.from(base64String, 'base64');
   const fileName = `${uuidv4()}.${mimeType.split('/')[1]}`;
   const filePath = path.join(__dirname, '..','public','uploads', fileName);
   
-  // Salva o arquivo no sistema de arquivos
+  // Salva o arquivo
   fs.writeFileSync(filePath, buffer);
   
-  // Retorna a URL acessível publicamente
-  return `/uploads/${fileName}`;
+  return `/uploads/${fileName}`; //URL para acessar
 };
