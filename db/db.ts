7bde6d5ef1c9a5e15b471a50ad3a491dd0ce02de
Lucas William <lucaswilliamml@gmail.com>
2024-08-29 23:20:56 -0300

    add docker and db changed to local

--- db/db.ts
+++ db/db.ts
@@ -1,11 +1,14 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.set("strictQuery", true);
+mongoose.set('strictQuery', true);
 
 const main = async () => {
-  await mongoose.connect(process.env.DB_URI);
-  console.log("DB Conectado com sucesso!");
+  await mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log('DB Conectado com sucesso!');
 };
 
 main().catch((err) => console.log(err));
