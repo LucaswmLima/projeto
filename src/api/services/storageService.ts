fa5853d285e3ebf1e0bf1fb6366f2a729c954b9e
Lucas William <lucaswilliamml@gmail.com>
2024-08-30 21:52:17 -0300

    add .env to ignore and .env.example

--- src/api/services/storageService.ts
+++ src/api/services/storageService.ts
@@ -3,15 +3,25 @@ import path from 'path';
 import { v4 as uuidv4 } from 'uuid';
 
 export const uploadToLocalStorage = (base64Data: string, mimeType: string): string => {
-  // Remove o prefixo
-  const base64String = base64Data.replace(/^data:image\/\w+;base64,/, '');
-  
-  const buffer = Buffer.from(base64String, 'base64');
-  const fileName = `${uuidv4()}.${mimeType.split('/')[1]}`;
-  const filePath = path.join(__dirname, '..','public','uploads', fileName);
-  
-  // Salva o arquivo
-  fs.writeFileSync(filePath, buffer);
-  
-  return `/uploads/${fileName}`; //URL para acessar
+  try {
+    // Remove o prefixo
+    const base64String = base64Data.replace(/^data:image\/\w+;base64,/, '');
+    const buffer = Buffer.from(base64String, 'base64');
+    const fileName = `${uuidv4()}.${mimeType.split('/')[1]}`;
+    const uploadDirectory = path.join(__dirname, '..', 'public', 'uploads');
+    const filePath = path.join(uploadDirectory, fileName);
+
+    // Cria o diretório se não existir
+    if (!fs.existsSync(uploadDirectory)) {
+      fs.mkdirSync(uploadDirectory, { recursive: true });
+    }
+
+    // Salva o arquivo
+    fs.writeFileSync(filePath, buffer);
+
+    return `/uploads/${fileName}`; // URL para acessar
+  } catch (error) {
+    console.error('Erro ao salvar o arquivo:', error);
+    throw error; // Re-lança o erro para que possa ser tratado em outro lugar
+  }
 };
